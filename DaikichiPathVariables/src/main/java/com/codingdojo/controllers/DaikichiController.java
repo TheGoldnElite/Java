da883a2f2463321bdbd85fa6d35b42444c86891e
TheGoldnElite <jsherbstritt@yahoo.com>
2022-06-13 22:28:37 -0400

    commit on main

--- DaikichiPathVariables/src/main/java/com/codingdojo/controllers/DaikichiController.java
+++ DaikichiPathVariables/src/main/java/com/codingdojo/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.codingdojo.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +13,31 @@ public class DaikichiController {
 		return "Welcome";
 	}
 	
-	@RequestMapping("")
+	@RequestMapping("/today")
 	public String today() {
-		return "Today you will find
+		return "Today you will find luck in all your endeavors";
 	}
 	
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
-		return "Today
+		return "Tomorrow an opportunity will aris,so be sure to open to new ideas!";
 	}
+	
+	@RequestMapping("/travel/{city}")
+		public String showcity(@PathVariable("city") String city) {
+			return "You will travel to " + city;
+			
+		}
+	@RequestMapping("/lotto/{number}")
+		public String number(@PathVariable("number") int number ) {
+			if (number%2 != 0) {
+				return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+			}
+			else {
+				return "You will take a grand journey in the near future, but be weary of tempting offers.";
+			}
+	
+	}
+	
 
-}
+};
